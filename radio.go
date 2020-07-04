bfebdc8512ad5573e2ab25a239f2592718fdd2b3
Maximilian Zinke <zinke@smark.de>
2020-07-05 00:42:01 +0200

    implement basic structure of channels with parents and children @ Channel Creation

--- radio.go
+++ radio.go
@@ -3,13 +3,17 @@ package radio
 import (
 	"errors"
 	"fmt"
+	"log"
+	"strings"
 	"time"
 )
 
 /* Represent a Channel where you can subscribe to. */
 type Channel struct {
-	name      string
+	parent    *Channel
+	path      string
 	listeners []*Listener
+	children  []*Channel
 }
 
 /* Listener is function which is executed when a event is dispatched through a channel */
@@ -24,34 +28,83 @@ type Event struct {
 
 var channelList []*Channel
 
-func NewChannel(name string) (*Channel, error) {
-	if isChannelExisting(name) {
-		return nil, errors.New(fmt.Sprintf("The channel %s is already existing, try listen to it instead.", name))
+// Create a new Channel to work with it
+func NewChannel(path string) (*Channel, error) {
+	if isChannelExisting(path) {
+		return nil, errors.New(fmt.Sprintf("The channel with the path %s is already existing, try listen to it instead.", path))
 	}
 
-	channel := &Channel{name, []*Listener{}}
+	parentChannelPath := getParentChannelPath(path)
+	var parentChannel = findParentChannel(parentChannelPath)
+	if parentChannel == nil && parentChannelPath != "" {
+		parentChannel, _ = NewChannel(parentChannelPath)
+	}
+
+	channel := &Channel{
+		parent: parentChannel,
+		path:   path,
+	}
 
 	registerChannel(channel)
+
+	if parentChannel != nil {
+		parentChannel.addChildren(channel)
+	}
 
 	return channel, nil
 }
 
-func (c *Channel) GetName() string {
-	return c.name
+// Returns the full path of the channel
+func (c *Channel) GetPath() string {
+	if c == nil {
+		return ""
+	}
+
+	return c.path
+}
+
+func (c *Channel) GetParent() *Channel {
+	return c.parent
+}
+
+func (c *Channel) addChildren(newChildren *Channel) {
+	if c.path == newChildren.path {
+		// Internally, that the same channel can never be added as a children to itself
+		log.Fatalln(fmt.Sprintf("You can't add the same channel to your own channel"))
+	}
+
+	c.children = append(c.children, newChildren)
 }
 
 func registerChannel(newChannel *Channel) {
 	channelList = append(channelList, newChannel)
 }
 
-func isChannelExisting(channelName string) bool {
-	var isExisting bool
+func findChannel(channelPath string) *Channel {
+	var foundChannel *Channel
 	for _, channel := range channelList {
-		if channel.name == channelName {
-			isExisting = true
+		if channel.path == channelPath {
+			foundChannel = channel
 			break
 		}
 	}
 
-	return isExisting
+	return foundChannel
+}
+
+func getParentChannelPath(channelPath string) string {
+	splicedPath := strings.Split(channelPath, ".")
+	if len(splicedPath) <= 1 {
+		return ""
+	}
+
+	return strings.Join(splicedPath[:(len(splicedPath)-1)], ".")
+}
+func findParentChannel(channelPath string) *Channel {
+	parentPath := getParentChannelPath(channelPath)
+	return findChannel(parentPath)
+}
+
+func isChannelExisting(channelPath string) bool {
+	return findChannel(channelPath) != nil
 }
--- tests/channel_test.go
+++ tests/channel_test.go
@@ -1,4 +1,4 @@
-package radio
+package radio_test
 
 import (
 	"github.com/mxzinke/radio"
@@ -18,24 +18,45 @@ func TestNewChannel(t *testing.T) {
 
 /* Should return first channel without error, but second channel should return error */
 func TestNewChannelDuplicate(t *testing.T) {
-	channelName := "test-channel.duplicate"
+	channelPath := "test-channel.duplicate"
 
-	channel1, err1 := radio.NewChannel(channelName)
-	channel2, err2 := radio.NewChannel(channelName)
+	channel1, err1 := radio.NewChannel(channelPath)
+	channel2, err2 := radio.NewChannel(channelPath)
 
 	/* -- first normal channel -- */
 	assert.NoError(t, err1, "Should not return error")
 	assert.IsType(t, &radio.Channel{}, channel1, "Should return channel object pointer")
 	/* -- second channel as duplicate -- */
 	assert.Error(t, err2, "Should return error")
-	assert.Contains(t, err2.Error(), channelName, "Should return error containing duplicated channel name")
+	assert.Contains(t, err2.Error(), channelPath, "Should return error containing duplicated channel path")
 	assert.Nil(t, channel2, "Should not return channel object (nil-pointer)")
 }
 
+/* Should return the correct channel name / path */
 func TestGetChannelName(t *testing.T) {
 	channelName := "test-channel.name"
+
+	channel, err := radio.NewChannel(channelName)
+	actualChannelName := channel.GetPath()
+
+	assert.NoError(t, err, "Should not return error")
+	assert.Equal(t, actualChannelName, channelName, "Should return channel name as a string")
+}
+
+/* Should return correct parent channels */
+func TestGetChannelParentRecursively(t *testing.T) {
+	channelName := "organization.system.subsystem.parent.channel"
+	parentChannelName := "organization.system.subsystem.parent"
+	subsystemChannelName := "organization.system.subsystem"
+
 	channel, err := radio.NewChannel(channelName)
 
 	assert.NoError(t, err, "Should not return error")
-	assert.Equal(t, channelName, channel.GetName(), "Should return channel name as a string")
+	assert.IsType(t, &radio.Channel{}, channel, "Should return channel object pointer")
+	parentChannel := channel.GetParent()
+	assert.IsType(t, &radio.Channel{}, parentChannel, "Should return channel object pointer for parent")
+	assert.Equal(t, parentChannelName, parentChannel.GetPath(), "Should return channel name of parent")
+	subsystemChannel := parentChannel.GetParent()
+	assert.IsType(t, &radio.Channel{}, subsystemChannel, "Should return channel object pointer for subsystem")
+	assert.Equal(t, subsystemChannelName, subsystemChannel.GetPath(), "Should return channel name of subsystem")
 }
